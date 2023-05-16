b18ce31457614598b11e9b7e2244eb606aae941b
Tvoje uživatelské jméno <Tvoje e-mailov adresa>
2023-05-16 14:01:03 +0200

    Xamarin

---
+++ DialogApp/DialogApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DialogApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            string osloveni = osloveniEntry.Text;
+            await DisplayAlert($"Ahoj {osloveni}!", "Vítej v první multiplatformní aplikaci.", "OK");
+        }
+        
+    }
+    
+}
